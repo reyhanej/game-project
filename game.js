938c6724844c479226ceca602949413e27186b7a
reyhane javivi <1100.reyhanej@gmail.com>
2021-07-24 21:02:41 +0430

    WIP on styles

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+import Modal from "./modal.js"
 class Game {
     options;
     selectedLevel;
---
+++ modal.js
@@ -0,0 +1,5 @@
+export default class Modal {
+    constructor(text) {
+        console.log('run from modal')
+    }
+}
